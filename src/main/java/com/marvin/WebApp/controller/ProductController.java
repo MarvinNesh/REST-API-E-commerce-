3d6430263033f9f638f5a8b0d2b51f99e9d3857c
Vhuhwavho <denganetshi@gmail.com>
2025-11-23 00:07:03 +0200

    get a single prod by their name

--- src/main/java/com/marvin/WebApp/controller/ProductController.java
+++ src/main/java/com/marvin/WebApp/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.marvin.WebApp.model.Product;
 import com.marvin.WebApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,9 @@ public class ProductController {
     @GetMapping("/products")
     public List<Product> getAllProducts(){
         return  service.getAllProducts();
+    }
+    @GetMapping("/product/{proID}")
+    public Product getProduct(@PathVariable("proID") Integer proID){
+        return service.getProductById(proID);
     }
 }
