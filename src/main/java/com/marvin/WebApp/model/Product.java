7e92bdf43e1dec1deb25bc3cef9064b90c5b40d2
Vhuhwavho <denganetshi@gmail.com>
2025-11-23 00:07:40 +0200

    generate id auto

--- src/main/java/com/marvin/WebApp/model/Product.java
+++ src/main/java/com/marvin/WebApp/model/Product.java
@@ -3,6 +3,8 @@ package com.marvin.WebApp.model;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,6 +18,7 @@ import java.util.Date;
 @NoArgsConstructor
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String description;
