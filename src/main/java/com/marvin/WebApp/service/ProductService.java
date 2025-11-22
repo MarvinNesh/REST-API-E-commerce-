25d301746d424f7786a30a9eda41d1f1c47a8cdc
Vhuhwavho <denganetshi@gmail.com>
2025-11-22 23:51:12 +0200

    added a method to get all products on db

--- src/main/java/com/marvin/WebApp/service/ProductService.java
+++ src/main/java/com/marvin/WebApp/service/ProductService.java
@@ -1,4 +1,18 @@
 package com.marvin.WebApp.service;
 
+import com.marvin.WebApp.model.Product;
+import com.marvin.WebApp.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+    @Autowired
+    private ProductRepo productRepo;
+    public List<Product> getAllProducts() {
+        return  productRepo.findAll() ;
+
+    }
 }
