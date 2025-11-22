27683bc1e598f4111cef1b7cb5b9dd19f20d6c2f
Vhuhwavho <denganetshi@gmail.com>
2025-11-23 00:36:14 +0200

    change Id from int to Integer

--- src/main/java/com/marvin/WebApp/model/Product.java
+++ src/main/java/com/marvin/WebApp/model/Product.java
@@ -19,7 +19,7 @@ import java.util.Date;
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
     private String name;
     private String description;
     private BigDecimal price;
