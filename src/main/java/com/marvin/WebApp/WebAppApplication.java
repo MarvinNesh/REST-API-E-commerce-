83e84b83d76a445aa9079a15105cb7574f07cf66
Vhuhwavho <denganetshi@gmail.com>
2025-11-22 21:04:14 +0200

    create project

--- src/main/java/com/marvin/WebApp/WebAppApplication.java
+++ src/main/java/com/marvin/WebApp/WebAppApplication.java
@@ -1,13 +1,16 @@
 package com.marvin.WebApp;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class WebAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebAppApplication.class, args);
+
+           ConfigurableApplicationContext context = SpringApplication.run(WebAppApplication.class, args);
 	}
 
 }
