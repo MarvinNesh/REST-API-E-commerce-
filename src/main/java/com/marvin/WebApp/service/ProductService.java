25d1134e7aa665ff91acb42bc8625436ff0f528f
Vhuhwavho <denganetshi@gmail.com>
2025-11-23 00:37:13 +0200

    added implementstion of adding product to db

--- src/main/java/com/marvin/WebApp/service/ProductService.java
+++ src/main/java/com/marvin/WebApp/service/ProductService.java
@@ -18,4 +18,7 @@ public class ProductService {
     public Product getProductById(Integer proID){
         return productRepo.findById(proID).get();
     }
+    public Product addProduct(Product product){
+        return productRepo.save(product);
+    }
 }
